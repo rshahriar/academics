0088270831ea87760d440e11d76b2556f5b6b2ee
Md Rakib Shahriar <rakib.shahriar@csebuet.org>
2015-11-18 13:33:50 -0600

    ws for machines

---
+++ machine-web-services/src/main/java/com/cpms/virtualization/webservices/MonitoringResource.java
@@ -0,0 +1,21 @@
+package com.cpms.virtualization.webservices;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Rakib on 11/18/2015.
+ */
+@Path("monitoring")
+public class MonitoringResource {
+
+    @GET
+    @Path("ultimaker")
+    @Produces("application/json")
+    public Response monitor() {
+        String dummy = "{\"name\" : \"rakib\"}";
+        return Response.ok(dummy).build();
+    }
+}
